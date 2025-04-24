e6f514f252d0a708c2836403a311b1eaa982a453
Jules Hemery <mrcouvbat@gmail.com>
2025-04-24 12:05:37 +0200

    Add new themes and update settings for Vesktop and update of dotfiles

--- share/dotfiles/.config/superfile/theme/update_theme.sh
+++ share/dotfiles/.config/superfile/theme/update_theme.sh
@@ -14,15 +14,15 @@ cat > "${HOME}/.config/superfile/theme/custom.toml" << EOF
 # Last updated: $(date)
 
 # ========= Border =========
-file_panel_border = "${color8}"
-sidebar_border = "${color0}"
-footer_border = "${color8}"
+file_panel_border = "${color5}"
+sidebar_border = "${color4}"
+footer_border = "${color4}"
 
 # ========= Border Active =========
-file_panel_border_active = "${color4}"
-sidebar_border_active = "${color1}"
-footer_border_active = "${color2}"
-modal_border_active = "${color7}"
+file_panel_border_active = "${color6}"
+sidebar_border_active = "${color6}"
+footer_border_active = "${color6}"
+modal_border_active = "${color6}"
 
 # ========= Background (bg) =========
 full_screen_bg = "${color0}"
@@ -32,11 +32,11 @@ footer_bg = "${color0}"
 modal_bg = "${color0}"
 
 # ========= Foreground (fg) =========
-full_screen_fg = "${color7}"
-file_panel_fg = "${color7}"
-sidebar_fg = "${color7}"
-footer_fg = "${color7}"
-modal_fg = "${color7}"
+full_screen_fg = "${color1}"
+file_panel_fg = "${color1}"
+sidebar_fg = "${color1}"
+footer_fg = "${color1}"
+modal_fg = "${color1}"
 
 # ========= Special Color =========
 cursor = "${color15}"
--- share/dotfiles/.config/wal/postrun.sh
+++ share/dotfiles/.config/wal/postrun.sh
@@ -2,3 +2,5 @@
 
 # Update Superfile theme
 ~/.config/superfile/theme/update_theme.sh
+
+cp ~/.cache/wal/colors-discord.css ~/.config/vesktop/themes/pywal-vencord.theme.css
